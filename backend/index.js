c09e84fc3514c46e2054ee0f67e0eda36b4817e9
Jonathon Lee <jonnyleealas@gmail.com>
2024-05-07 08:41:16 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 // routes
 
-app.get('/', async (req, res) => {
+app.get('/books', async (req, res) => {
     const allBooks = await Book.find({})
     res.status(201).json({allBooks})
 })
@@ -34,6 +34,28 @@ app.get('/books/:id', async (req, res) => {
     } catch (error) {
         response.status(500).send({message: error.message})
     }
+})
+
+app.put('/books/:id', async (req, res) => {
+
+    try{
+    if(!req.body.title || !req.body.author || !req.body.genre){
+        return res.status(400).send({message: 'send all required fields: title, author, publisher'})
+    }
+
+    const {id} = req.params
+
+    const result = await Book.findByIdAndUpdate(id, req.body)
+
+    if(!result){
+        return res.status(404).json({message: 'book not found'})
+    } 
+    return res.status(200).json({result})
+    
+} catch (error){
+        res.status(500).send({message: error.message})
+    }
+
 })
 
 app.delete('/books/:id', async (req, res) => {

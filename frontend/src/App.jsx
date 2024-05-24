03a3e6e28b215c36a62255de309d5aa0218a636a
Jonathon Lee <jonnyleealas@gmail.com>
2024-05-24 17:08:14 -0500

    created react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import CreateBook from './pages/CreateBooks'
+import UpdateBook from './pages/UpdateBook'
+import DeleteBook from './pages/DeleteBook'
+import GetBook from './pages/GetBook'
+import Home from './pages/Home'
+
+
 
 export const App = () => {
   return (
-    <div>Apfffp</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<GetBook />} />
+      <Route path='/books/edit/:id' element={<UpdateBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
 

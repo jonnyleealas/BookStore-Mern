048428936cf1cb14d122ac84a999a1154daa0c02
Jonathon Lee <jonnyleealas@gmail.com>
2024-05-25 22:05:41 -0500

    react routes working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import CreateBook from './pages/CreateBook.jsx'
+import DeleteBook from './pages/DeleteBook.jsx'
+import EditBook from './pages/EditBook.jsx'
+import ShowBook from './pages/ShowBook.jsx'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+    </Routes>
   )
 }
 

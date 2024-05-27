7e922d3556dcd1cdace4796a3428f7e72c29ab1c
Jonathon Lee <jonnyleealas@gmail.com>
2024-05-27 17:55:10 -0500

    map cannot iterate through objects
    convert an object into an array and it can work

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,14 +15,15 @@ const Home = () => {
     axios
     .get('http://localhost:5555/books')
     .then((response) => {
-      setBooks(response.data.data)
+      setBooks(response.data)
+      console.log(books, 'fuckkkk')
       setLoading(false)
     })
     .catch((error) => {
       console.log(error)
     })
   }, [])
-
+  
   return (
     <div className='p-4'>
       <div className='flex justify-between items-center'>
@@ -40,12 +41,40 @@ const Home = () => {
               <th className='border border-slate-600 rounded-md'>No</th>
               <th className='border border-slate-600 rounded-md'>Title</th>
               <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Publisher Year</th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>Genre</th>
               <th className='border border-slate-600 rounded-md'>Operations</th>
             </tr>
           </thead>
           <tbody>
-
+            {[books].map((book, index) => (
+              <tr key={book._id} className='h-8'>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {index + 1}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {book.title}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                {book.author}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {book.genre}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                <div className='flex justify-center gap-x-4'>
+                  <Link to={`/books/details/${book._id}`}>
+                    <BsInfoCircle className='text-2xl text-green-800' />
+                  </Link>
+                  <Link to={`/books/edit/${book._id}`}>
+                    <AiOutlineEdit className='text-2xl text-yellow-800' />
+                  </Link>
+                  <Link to={`/books/delete/${book._id}`}>
+                    <MdOutlineDelete className='text-2xl text-red-800' />
+                  </Link>
+                </div>
+              </td>
+            </tr>
+            ))}
           </tbody>
         </table>
       )}

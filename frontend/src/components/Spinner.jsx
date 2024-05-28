b1dbfb17516cccc88c47e992397ef9c2d827f5ff
Jonathon Lee <jonnyleealas@gmail.com>
2024-05-27 22:10:03 -0500

    added create req on front end to backend
    axios to localhost:backendport/route

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 export const Spinner = () => {
   return (
-    <div className='animate-ping 2-16 h-16 m-8 rounded-full bg-sky-600'>Spinner</div>
+    <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'>Loading...</div>
   )
 }
+ 
